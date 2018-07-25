2b7116c924ea4e4aff7731f15dccdd06ea4a6a44
Charlie Crouse <charlietcrouse@gmail.com>
2018-07-25 10:59:06 -0400

    Setup client

---
+++ src/components/App/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+
+export class App extends React.Component {
+  render() {
+    return (
+      <div className="app-container">
+        <h2>App</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
